f1aa3825bf9160ff25c1bd2ff6900a8cc9de9cca
Ventura-Jr <ventura_jr@outlook.com>
2023-04-30 14:12:06 -0300

    primeiro commit do projeto

---
+++ script.js
@@ -0,0 +1,17 @@
+document
+    .getElementById("formulario-cadastro")
+    .addEventListener('submit', (event) => {
+        event.preventDefault();
+        const nome = document.getElementById("nome").value;
+        const email = document.getElementById("email").value;
+        const senha = document.getElementById("senha").value;
+        const form = { nome, email, senha }
+        //alert(JSON.stringify(form, null, 2))
+        fetch('http://localhost:3000/cadastro', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(form)
+        })
+    })
